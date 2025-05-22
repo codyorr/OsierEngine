42d723b9375ed16b7f718f8227685be711a7d980
cody <codyorr.contact@gmail.com>
2025-05-22 16:27:50 -0500

    Implemented CoreListener Interface
    CoreListener will hold all the core events of the engine

--- src/org/osier/listeners/CoreListener.java
+++ src/org/osier/listeners/CoreListener.java
@@ -1,5 +1,22 @@
 package org.osier.listeners;
 
-public class CoreListener {
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 
+public interface CoreListener {
+	//setup defaults (as to not force users to implement all methods at once)
+	default public void load() {}
+	default public void update() {}
+	default public void render(Graphics2D g) {}
+	default public void mouseClicked(MouseEvent e, boolean processed) {}
+	default public void mousePressed(MouseEvent e, boolean processed) {}
+	default public void mouseReleased(MouseEvent e, boolean processed) {}
+	default public void mouseMoved(MouseEvent e, boolean isDragging, boolean processed) {}
+	default public void mouseScrolled(MouseEvent e, boolean processed) {}
+	default public void keyPressed(KeyEvent e, boolean processed) {}
+	default public void keyReleased(KeyEvent e, boolean processed) {}
+	default public void windowResized(int width, int height) {}
+	default public void dispose() {}
+	
 }
