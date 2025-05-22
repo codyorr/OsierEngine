4679c681e278c37679c4eb366fbf1fc92fc848e1
cody <codyorr.contact@gmail.com>
2025-05-22 16:42:03 -0500

    Started BaseGUIObject

--- src/org/osier/ui/BaseGUIObject.java
+++ src/org/osier/ui/BaseGUIObject.java
@@ -1,5 +1,18 @@
 package org.osier.ui;
 
-public class BaseGUIObject {
+import java.util.UUID;
 
+public class BaseGUIObject {
+	
+	
+	protected String name;
+	protected String id;
+	protected GUIChildren children;
+	protected int x,y,width,height;
+	
+	protected BaseGUIObject() {
+		name = "BaseGUIObject";
+		id = UUID.randomUUID().toString();
+		children = new GUIChildren();
+	}
 }
