761a072cdf7efd1d24dc8ad6600e271f99220344
cody <codyorr.contact@gmail.com>
2025-05-22 17:03:07 -0500

    Added MouseEntered and MouseExited

--- src/org/osier/listeners/CoreListener.java
+++ src/org/osier/listeners/CoreListener.java
@@ -12,11 +12,13 @@ public interface CoreListener {
 	default public void mouseClicked(MouseEvent e, boolean processed) {}
 	default public void mousePressed(MouseEvent e, boolean processed) {}
 	default public void mouseReleased(MouseEvent e, boolean processed) {}
-	default public void mouseMoved(MouseEvent e, boolean isDragging, boolean processed) {}
+	default public void mouseMoved(MouseEvent e, boolean isDragging) {}
 	default public void mouseScrolled(MouseEvent e, boolean processed) {}
 	default public void keyPressed(KeyEvent e, boolean processed) {}
 	default public void keyReleased(KeyEvent e, boolean processed) {}
 	default public void windowResized(int width, int height) {}
+	default public void mouseEntered(MouseEvent e) {}
+	default public void mouseExited(MouseEvent e) {}
 	default public void dispose() {}
 	
 }
