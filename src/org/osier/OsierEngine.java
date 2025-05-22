e5638682ceabacb0a3c167aad393f466b27a9221
cody <codyorr.contact@gmail.com>
2025-05-22 16:39:45 -0500

    started the Engine

--- src/org/osier/OsierEngine.java
+++ src/org/osier/OsierEngine.java
@@ -1,5 +1,35 @@
 package org.osier;
 
-public class OsierEngine {
+import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.image.BufferStrategy;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+import org.osier.listeners.CoreListener;
+import org.osier.ui.Window;
+
+public class OsierEngine implements CoreListener{
+	
+	private Graphics2D g;
+	public Window window;
+	private BufferStrategy bs;
+	private Thread renderThread;
+	private GraphicsDevice display;
+	private ConcurrentLinkedQueue inputQueue;
+	
+	public OsierEngine(String title, int width, int height, boolean decorated) {
+		
+	}
+	
+	public void start() {
+		
+	}
+	
+	public void stop() {
+		
+	}
+	
+	
+	
+	
 }
